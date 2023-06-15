1f37b0619ce67e5749b8ac31ae282ba1fff6052e
MeseryGamer <meserygamer@outlook.com>
2023-06-15 12:48:40 +0300

    Переосмыслен подход к хранению стилей и behaviors

---
+++ ProjectBOX/Authorization/AuthorizationWaterMarkAndBehaviors.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace ProjectBOX.Authorization
+{
+    public class WaterMarkTextBox : TextBox
+    {
+        #region Fields
+
+        private const string _defaultWatermark = "None";
+
+        public static readonly DependencyProperty WatermarkTextProperty = DependencyProperty.Register("WatermarkText", typeof(string), typeof(WaterMarkTextBox), new UIPropertyMetadata(string.Empty, OnWatermarkTextChanged));
+
+        #endregion
+
+        #region Constructor(s)
+        public WaterMarkTextBox()
+          : this(_defaultWatermark)
+        {
+        }
+        public WaterMarkTextBox(string watermark)
+        {
+            WatermarkText = watermark;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string WatermarkText
+        {
+            get { return (string)GetValue(WatermarkTextProperty); }
+            set { SetValue(WatermarkTextProperty, value); }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public static void OnWatermarkTextChanged(DependencyObject box, DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+        #endregion
+    }
+    public class PasswordBoxBehavior : Behavior<PasswordBox>
+    {
+        public static readonly DependencyProperty LenghPasswordProperty;
+        public static readonly DependencyProperty KeyBoardFocusProperty;
+
+        static PasswordBoxBehavior()
+        {
+            KeyBoardFocusProperty = DependencyProperty.Register("KeyBoardFocus", typeof(bool), typeof(PasswordBoxBehavior));
+            LenghPasswordProperty = DependencyProperty.Register("LenghPassword", typeof(int?), typeof(PasswordBoxBehavior));
+        }
+
+        public int? LenghPassword
+        {
+            get { return (int?)GetValue(LenghPasswordProperty); }
+            set { SetValue(LenghPasswordProperty, value); }
+        }
+
+        public bool KeyBoardFocus
+        {
+            get { return (bool)GetValue(KeyBoardFocusProperty); }
+            set { SetValue(KeyBoardFocusProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            // Присоединение обработчиков событий            
+            this.AssociatedObject.PasswordChanged += AssociatedObject_PasswordChanged;
+            this.AssociatedObject.IsKeyboardFocusWithinChanged += AssociatedObject_IsKeyboardFocusWithinChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            // Удаление обработчиков событий
+            this.AssociatedObject.MouseLeftButtonDown -= AssociatedObject_PasswordChanged;
+            this.AssociatedObject.IsKeyboardFocusWithinChanged -= AssociatedObject_IsKeyboardFocusWithinChanged;
+        }
+
+        private void AssociatedObject_IsKeyboardFocusWithinChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            KeyBoardFocus = this.AssociatedObject.IsKeyboardFocusWithin;
+        }
+
+        private void AssociatedObject_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (AssociatedObject.Password is null)
+            {
+                LenghPassword = null;
+                return;
+            }
+            LenghPassword = AssociatedObject.Password.Length;
+        }
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+        }
+    }
+}
--- ProjectBOX/Authorization/RegistrationUC/RegistrationWindowViewModel.cs
+++ ProjectBOX/Authorization/RegistrationUC/RegistrationWindowViewModel.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjectBOX.Authorization.RegistrationUC
 {
-    internal class RegistrationWindowViewModel
+    public class RegistrationWindowViewModel
     {
+        private int? _lenghPassword;
+        private bool _keyBoardFocusStatus;
+
+        public int? LenghPassword
+        {
+            get => _lenghPassword;
+            set
+            {
+                _lenghPassword = value;
+                OnPropertyChanged("LenghPassword");
+            }
+        }
+
+        public bool KeyBoardFocusStatus
+        {
+            get => _keyBoardFocusStatus;
+            set
+            {
+                _keyBoardFocusStatus = value;
+                OnPropertyChanged("KeyBoardFocusStatus");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }

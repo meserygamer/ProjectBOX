6a27bde7015a6a0687699892ebec00316a4245fe
meserygamer <meserygamer@gmail.com>
2023-06-20 22:27:16 +0300

    Добавлен стиль для кнопки "Все предметы"

--- ProjectBOX/ItemsWindow/ItemsWindowView.xaml.cs
+++ ProjectBOX/ItemsWindow/ItemsWindowView.xaml.cs
@@ -23,10 +23,5 @@ namespace ProjectBOX.ItemsWindow
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
---
+++ ProjectBOX/ItemsWindow/ItemsWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using ProjectBOX.Authorization.LoginUC;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProjectBOX.ItemsWindow
+{
+    public class ItemsWindowViewModel : INotifyPropertyChanged
+    {
+        private bool _userChoseSeeAllObject;
+        private RelayCommand _userClickOnSeeAllObjectButton;
+
+        public RelayCommand UserClickOnSeeAllObjectButton
+        {
+            get
+            {
+                return _userClickOnSeeAllObjectButton ??
+                  (_userClickOnSeeAllObjectButton = new RelayCommand(obj =>
+                  {
+                      UserChoseSeeAllObject = true;
+                  }));
+            }
+        }
+
+        public bool UserChoseSeeAllObject
+        { 
+            get => _userChoseSeeAllObject;
+            set
+            {
+                _userChoseSeeAllObject = value;
+                OnPropertyChanged("UserChoseSeeAllObject");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}

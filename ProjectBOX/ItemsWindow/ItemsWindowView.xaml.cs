2558846c8d13f6a4c5467d6a23b7809326ae64b6
MeseryGamer <meserygamer@outlook.com>
2023-06-20 14:01:43 +0300

    Добавлен поисковой бар и изменено ItemsWindow

--- ProjectBOX/ItemsWindow/ItemsWindowView.xaml.cs
+++ ProjectBOX/ItemsWindow/ItemsWindowView.xaml.cs
@@ -23,5 +23,10 @@ namespace ProjectBOX.ItemsWindow
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
---
+++ ProjectBOX/ItemsWindow/SearchFieldUC/SearchFieldUCView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using ProjectBOX.ItemsWindow.UserAreaUC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectBOX.ItemsWindow.SearchFieldUC
+{
+    /// <summary>
+    /// Логика взаимодействия для SearchFieldUCView.xaml
+    /// </summary>
+    public partial class SearchFieldUCView : UserControl
+    {
+        public static readonly DependencyProperty SearchQueryProperty = DependencyProperty.Register("SearchQuery", typeof(string), typeof(SearchFieldUCView));
+        public static readonly DependencyProperty ClickOnSettingsButtonProperty = DependencyProperty.Register("ClickOnSettingsButton", typeof(RelayCommand), typeof(SearchFieldUCView));
+        public static readonly DependencyProperty ClickOnSearchButtonProperty = DependencyProperty.Register("ClickOnSearchButton", typeof(RelayCommand), typeof(SearchFieldUCView));
+
+        public string SearchQuery
+        {
+            get => (string)GetValue(SearchQueryProperty);
+            set => SetValue(SearchQueryProperty, value);
+        }
+
+        public RelayCommand ClickOnSettingsButton
+        {
+            get => (RelayCommand)GetValue(ClickOnSettingsButtonProperty);
+            set => SetValue(ClickOnSettingsButtonProperty, value);
+        }
+
+        public RelayCommand ClickOnSearchButton
+        {
+            get => (RelayCommand)GetValue(ClickOnSearchButtonProperty);
+            set => SetValue(ClickOnSearchButtonProperty, value);
+        }
+
+        public SearchFieldUCView()
+        {
+            InitializeComponent();
+        }
+    }
+}

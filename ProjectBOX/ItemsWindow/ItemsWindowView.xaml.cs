43064b055e332fa0370bfe1538c683b23b96625c
MeseryGamer <meserygamer@outlook.com>
2023-06-22 13:46:37 +0300

    Добавлен тестовый материал, для тестирования функционала datagrid

--- ProjectBOX/ItemsWindow/ItemsWindowView.xaml.cs
+++ ProjectBOX/ItemsWindow/ItemsWindowView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ProjectBOX.EntityFrameworkModelFiles;
 
 namespace ProjectBOX.ItemsWindow
 {
@@ -19,9 +21,18 @@ namespace ProjectBOX.ItemsWindow
     /// </summary>
     public partial class ItemsWindowView : Window
     {
+        List<CompleteTask> completeTasks;
+
         public ItemsWindowView()
         {
             InitializeComponent();
+            GetAllCompleteTask();
+        }
+
+        public async void GetAllCompleteTask()
+        {
+            await Task.Run(() => {completeTasks = (new ProjectBoxDbContext().CompleteTasks.ToList()); });
+            MainGrid.ItemsSource = completeTasks;
         }
     }
 }

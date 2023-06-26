ac3bd8c7ed598df031ca5797e9aef65100b05906
meserygamer <meserygamer@gmail.com>
2023-06-26 22:49:27 +0300

    CreateContainerForm Готова

---
+++ ProjectBOX/ItemsWindowForms/CreateContainerForm/CreateContainerFormModel.cs
@@ -0,0 +1,79 @@
+﻿using ProjectBOX.EntityFrameworkModelFiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBOX.ItemsWindowForms.CreateContainerForm
+{
+    public class Validator
+    {
+        private string _itemName;
+        private bool _isValid = true;
+
+        public Validator(string? itemName)
+        {
+            _itemName = itemName ?? "";
+        }
+
+        public Validator CheckItemNameOnEmpty()
+        {
+            if (_itemName.Length == 0)
+            {
+                _isValid = false;
+            }
+            return this;
+        }
+
+        public bool Validation() => _isValid;
+    }
+
+    public class CreateContainerFormInteractionWithDataBase
+    {
+        private static CreateContainerFormInteractionWithDataBase _instance;
+
+        #region CategoryName string
+        private string _categoryName;
+
+        public string CategoryName
+        {
+            get { return _categoryName; }
+            set { _categoryName = value; }
+        }
+        #endregion
+        #region CategoryDescription string
+        private string _categoryDescription;
+
+        public string CategoryDescription
+        {
+            get { return _categoryDescription; }
+            set { _categoryDescription = value; }
+        }
+        #endregion
+
+        private CreateContainerFormInteractionWithDataBase() { }
+
+        public async void AddCategoryInDataBaseAsync()
+        {
+            await Task.Run(() =>
+            {
+                ContainerDatum Container = new() {ContainerName = CategoryName, Description = CategoryDescription};
+                using(ProjectBoxDbContext DB = new())
+                {
+                    DB.ContainerData.Add(Container);
+                    DB.SaveChanges();
+                }
+            });
+        }
+
+        public static CreateContainerFormInteractionWithDataBase GetExemplar()
+        {
+            if(_instance is null)
+            {
+                return _instance = new CreateContainerFormInteractionWithDataBase();
+            }
+            else return _instance;
+        }
+    }
+}
--- ProjectBOX/ItemsWindowForms/CreateContainerForm/CreateContainerFormViewModel.cs
+++ ProjectBOX/ItemsWindowForms/CreateContainerForm/CreateContainerFormViewModel.cs
@@ -1,14 +1,75 @@
-﻿using System;
+﻿using ProjectBOX.ItemsWindowForms.EditingItemForm;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ProjectBOX.ItemsWindowForms.CreateContainerForm
 {
     public class CreateContainerFormViewModel : INotifyPropertyChanged
     {
+        #region CategoryName string
+        private string _categoryName;
 
+        public string CategoryName
+        {
+            get { return _categoryName; }
+            set {
+                _categoryName = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+        #region CategoryDescription string
+        private string _categoryDescription;
+
+        public string CategoryDescription
+        {
+            get { return _categoryDescription; }
+            set
+            {
+                _categoryDescription = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region CreateCategoryClick RelayCommand
+        private RelayCommand _createCategoryClick;
+
+        public RelayCommand CreateCategoryClick
+        {
+            get
+            {
+                return _createCategoryClick ??
+                  (_createCategoryClick = new RelayCommand(obj =>
+                  {
+                      if (new Validator(_categoryName).CheckItemNameOnEmpty().Validation())
+                      {
+                          CreateContainerFormInteractionWithDataBase.GetExemplar().CategoryName = _categoryName;
+                          CreateContainerFormInteractionWithDataBase.GetExemplar().CategoryDescription = _categoryDescription;
+                          CreateContainerFormInteractionWithDataBase.GetExemplar().AddCategoryInDataBaseAsync();
+                      }
+                      else
+                      {
+                          MessageBox.Show("Поле названия контейнера - пустое, контейнер не добавлен");
+                      }
+                  }));
+            }
+        }
+        #endregion
+
+        #region INotifyPropertyChanged Realize
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        #endregion
     }
 }

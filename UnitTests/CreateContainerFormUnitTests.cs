47e889df0a9b82200c17cdde342b7ad7f02c8c30
MeseryGamer <meserygamer@outlook.com>
2023-07-02 18:22:26 +0300

    Model окна создания контейнера покрыт тестами

---
+++ UnitTests/CreateContainerFormUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectBOX.ItemsWindowForms.CreateContainerForm;
+
+namespace UnitTests
+{
+    public class CreateContainerFormUnitTests
+    {
+        const string CorrectNameOfContainer = "Stock 1";
+        [Fact]
+        public void CheckMothodOfCheckContainerNameOnEmpty1()
+        {
+            Assert.False((new Validator(null)).CheckItemNameOnEmpty().Validation());
+        }
+
+        [Fact]
+        public void CheckMothodOfCheckContainerNameOnEmpty2()
+        {
+            Assert.False((new Validator("")).CheckItemNameOnEmpty().Validation());
+        }
+
+        [Fact]
+        public void CheckMothodOfCheckContainerNameOnEmpty3()
+        {
+            Assert.True((new Validator(CorrectNameOfContainer)).CheckItemNameOnEmpty().Validation());
+        }
+    }
+}
